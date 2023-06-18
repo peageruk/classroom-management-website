7c57f055fbbcbcb5210f765d723331cef6f58cce
peageruk <98ducanhdo@gmail.com>
2023-06-18 11:15:10 +0700

    react-redux

--- react-app/src/components/Counter.js
+++ react-app/src/components/Counter.js
@@ -1,24 +1,23 @@
 import { useState } from "react";
 
 function Counter() {
-    const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-    const increase = () => {
-        setCounter(prevCounter => prevCounter + 1)
-    }
+  const increase = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
 
-    const decrease = () => {
-        setCounter(prevCounter => prevCounter - 1)
-    }
+  const decrease = () => {
+    setCounter((prevCounter) => prevCounter - 1);
+  };
 
-    return (
-        <div>
-            <p>Value: {counter}</p>
-            <button onClick={increase}>Increase</button>
-            <button onClick={decrease}>Decrease</button>
-        </div>
-    )
+  return (
+    <div>
+      <p>Value: {counter}</p>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+    </div>
+  );
 }
 
 export default Counter;
-
